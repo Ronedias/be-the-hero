253b3f6a9041910ba08608eb15f90b986963e077
RONE G D FARIAS <ronecosac@gmail.com>
2024-08-14 00:07:26 -0300

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://roneproducoes.ddns.net:8243/api',
+    baseURL: 'https://roneproducoes.ddns.net:8243',
 })
 
 export default api;
